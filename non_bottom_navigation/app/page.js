8df9d7c2643e04126efe725429e54b99739a4c27
99-zziy <wldud8229@gmail.com>
2023-06-05 12:59:30 +0900

    textarea 추가

--- non_bottom_navigation/app/page.js
+++ non_bottom_navigation/app/page.js
@@ -3,6 +3,10 @@ export default function Community() {
     <div>
       <h1>Community</h1>
       <input inputmode="text" />
+      <br />
+      <br />
+      <br />
+      <textarea input="text" />
     </div>
   );
 }
